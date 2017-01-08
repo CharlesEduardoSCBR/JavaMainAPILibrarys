2c419009371c838c8d9cd481728848f5081e4181
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-08 15:37:24 -0200

    Exercicio 7 - Resposta...

--- Java Jr/src/br/com/alura/java3/main/aula4/EntradaDeUmArquivo.java
+++ Java Jr/src/br/com/alura/java3/main/aula4/EntradaDeUmArquivo.java
@@ -1,21 +1,24 @@
 package br.com.alura.java3.main.aula4;
 
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class EntradaDeUmArquivo {
 
 	public static void main(String[] args) throws IOException {
-		//InputStream is = new FileInputStream("arquivo.txt");
-		Scanner entrada = new Scanner(System.in);
 
-		System.out.println("Digite sua mensagem:");
-		while (entrada.hasNextLine()) {
-			System.out.println(entrada.nextLine());
-		}
-		entrada.close();
+		String pathArquivo = "C:/Users/charl_000/Documents/Cursos/Alura/Trilhas/Java III/Java Jr/Java Jr/src/br/com/alura/java3/arq/arquivo.txt";
+
+		Scanner entrada = new Scanner(new FileInputStream(pathArquivo));
+        PrintStream saida = new PrintStream(new FileOutputStream("copia.txt"));
+        while (entrada.hasNextLine()) {
+            saida.println(entrada.nextLine());
+        }
+        saida.close();
+        entrada.close();
 	}
-
 }
