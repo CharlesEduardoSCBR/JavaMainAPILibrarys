903d5ffc4736e6daa41758a7b6baeebd46bb3121
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-08 15:42:35 -0200

    Exercicio 8 - alteração de entrada, mais uma amostra das vantagens do polimorfismo.

--- Java Jr/src/br/com/alura/java3/main/aula4/EntradaDeUmArquivo.java
+++ Java Jr/src/br/com/alura/java3/main/aula4/EntradaDeUmArquivo.java
@@ -1,7 +1,5 @@
 package br.com.alura.java3.main.aula4;
 
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -11,9 +9,7 @@ public class EntradaDeUmArquivo {
 
 	public static void main(String[] args) throws IOException {
 
-		String pathArquivo = "C:/Users/charl_000/Documents/Cursos/Alura/Trilhas/Java III/Java Jr/Java Jr/src/br/com/alura/java3/arq/arquivo.txt";
-
-		Scanner entrada = new Scanner(new FileInputStream(pathArquivo));
+		Scanner entrada = new Scanner(System.in);
         PrintStream saida = new PrintStream(new FileOutputStream("copia.txt"));
         while (entrada.hasNextLine()) {
             saida.println(entrada.nextLine());
