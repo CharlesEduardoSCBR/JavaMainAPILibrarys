ed077cead3abd7612aa75d2a278c4fb5b84e5cdd
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-08 14:54:03 -0200

    Exercicio 3 - Leitura de arquvi ocom FileInputStream

--- Java Jr/src/br/com/alura/java3/main/aula4/JavaIO.java
+++ Java Jr/src/br/com/alura/java3/main/aula4/JavaIO.java
@@ -1,6 +1,7 @@
 package br.com.alura.java3.main.aula4;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -9,18 +10,19 @@ public class JavaIO {
 
 	public static void main(String[] args) throws IOException {
 
-		InputStream psLeitor = System.in;
+		InputStream psLeitor = new FileInputStream(
+				"C:/Users/charl_000/Documents/Cursos/Alura/Trilhas/Java III/Java Jr/Java Jr/src/br/com/alura/java3/arq/arquivo.txt");
 		InputStreamReader isr = new InputStreamReader(psLeitor);
 		BufferedReader br = new BufferedReader(isr);
 
 		System.out.println("Digite sua mensagem:");
 		String linha = br.readLine(); // primeira linha
-		
+
 		while (linha != null) {
 			System.out.println(linha);
 			linha = br.readLine();
 		}
-		
+
 		br.close();
 	}
 }
