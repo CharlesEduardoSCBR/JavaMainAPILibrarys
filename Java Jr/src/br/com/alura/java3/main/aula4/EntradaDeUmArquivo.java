bcbc2485874b8e083ba927d13be5bcb4699f8da3
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-08 15:23:18 -0200

    Exercicio 6 - Classe Scanner

---
+++ Java Jr/src/br/com/alura/java3/main/aula4/EntradaDeUmArquivo.java
@@ -0,0 +1,21 @@
+package br.com.alura.java3.main.aula4;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class EntradaDeUmArquivo {
+
+	public static void main(String[] args) throws IOException {
+		InputStream is = new FileInputStream("arquivo.txt");
+		Scanner entrada = new Scanner(is);
+
+		System.out.println("Digite sua mensagem:");
+		while (entrada.hasNextLine()) {
+			System.out.println(entrada.nextLine());
+		}
+		entrada.close();
+	}
+
+}
