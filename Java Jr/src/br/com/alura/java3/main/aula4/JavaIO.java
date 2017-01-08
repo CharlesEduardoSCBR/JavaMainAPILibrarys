baf95c9e95e5fe574d9f64657d6b8afc0f114146
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-08 15:18:58 -0200

    Exercicio 5 - Forma de escrita curta, para declaração das variaveis.

--- Java Jr/src/br/com/alura/java3/main/aula4/JavaIO.java
+++ Java Jr/src/br/com/alura/java3/main/aula4/JavaIO.java
@@ -3,17 +3,15 @@ package br.com.alura.java3.main.aula4;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class JavaIO {
 
 	public static void main(String[] args) throws IOException {
 
-		InputStream psLeitor = new FileInputStream(
-				"C:/Users/charl_000/Documents/Cursos/Alura/Trilhas/Java III/Java Jr/Java Jr/src/br/com/alura/java3/arq/arquivo.txt");
-		InputStreamReader isr = new InputStreamReader(psLeitor);
-		BufferedReader br = new BufferedReader(isr);
+		String pathArquivo = "C:/Users/charl_000/Documents/Cursos/Alura/Trilhas/Java III/Java Jr/Java Jr/src/br/com/alura/java3/arq/arquivo.txt";
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(pathArquivo)));
 
 		System.out.println("Digite sua mensagem:");
 		String linha = br.readLine(); // primeira linha
