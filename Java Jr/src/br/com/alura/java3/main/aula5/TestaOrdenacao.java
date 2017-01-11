ba999b18dceb64ad2c9d4c7c6fd17ed92c8b2c9e
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-10 22:11:05 -0200

    Aula 5 - Exercicio 7

--- Java Jr/src/br/com/alura/java3/main/aula5/TestaOrdenacao.java
+++ Java Jr/src/br/com/alura/java3/main/aula5/TestaOrdenacao.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 import br.com.alura.java3.modelos.Conta;
 import br.com.alura.java3.modelos.ContaPoupanca;
@@ -18,8 +19,11 @@ public class TestaOrdenacao {
 		for (int i = 0; i < 20; i++) {
 			ContaPoupanca conta = new ContaPoupanca();
 			String nome = "Conta" + i;
+			Random r = new Random();
+			
 			conta.setNumero(i);
 			conta.setNome(nome);
+			conta.deposita(r.nextInt());
 			contas.add(conta);
 		}
 
