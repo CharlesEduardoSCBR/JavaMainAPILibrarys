676f2f6c8381d2d6410d719b19dbc193af43e850
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-11 20:54:40 -0200

    Aula 5 - Exercicio 13 - continuacao

--- Java Jr/src/br/com/alura/java3/main/aula5/TestaPerformanceDeAdicionarNaPrimeiraPosicao.java
+++ Java Jr/src/br/com/alura/java3/main/aula5/TestaPerformanceDeAdicionarNaPrimeiraPosicao.java
@@ -1,12 +1,13 @@
 package br.com.alura.java3.main.aula5;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class TestaPerformanceDeAdicionarNaPrimeiraPosicao {
 
 	public static void main(String[] args) {
-		System.out.println("Iniciando...");
+		System.out.println("Iniciando LinkedList...");
         long inicio = System.currentTimeMillis();
 
         // trocar depois por ArrayList                
@@ -18,6 +19,20 @@ public class TestaPerformanceDeAdicionarNaPrimeiraPosicao {
 
         long fim = System.currentTimeMillis();
         double tempo = (fim - inicio) / 1000.0;
+        System.out.println("Tempo gasto: " + tempo);
+        
+        System.out.println("Iniciando LinkedList...");
+        inicio = System.currentTimeMillis();
+
+        // trocar depois por ArrayList                
+        List<Integer> teste2 = new ArrayList<>();
+
+        for (int i = 0; i < 30000; i++) {
+            teste2.add(0, i);
+        }
+
+        fim = System.currentTimeMillis();
+        tempo = (fim - inicio) / 1000.0;
         System.out.println("Tempo gasto: " + tempo);
 	}
 
