b34128b96bd6763a6942381e692f22e95d2996d6
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-11 21:18:34 -0200

    Aula 5 - Exercicio 16 - Equals and HashCode

--- Java Jr/src/br/com/alura/java3/modelos/Conta.java
+++ Java Jr/src/br/com/alura/java3/modelos/Conta.java
@@ -12,14 +12,29 @@ public abstract class Conta {
 				" com saldo R$" + this.saldo;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		Conta outraConta = (Conta) obj;
+	public abstract void atualiza(double taxa);
 
-		return (this.numero == outraConta.numero && this.nome == outraConta.nome);
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + numero;
+		return result;
 	}
 
-	public abstract void atualiza(double taxa);
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (numero != other.numero)
+			return false;
+		return true;
+	}
 
 	public double getSaldo() {
 		return saldo;
