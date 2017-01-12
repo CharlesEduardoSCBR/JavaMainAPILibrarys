3f76cfabadf7847f6a54baa6c0ffe23bcbd570f2
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-11 22:20:28 -0200

    Aula 5 - Exercicio 19 - ArrayList decrescente

--- Java Jr/src/br/com/alura/java3/main/aula5/TestaOrdemDecrescenteExerc18.java
+++ Java Jr/src/br/com/alura/java3/main/aula5/TestaOrdemDecrescenteExerc18.java
@@ -1,19 +1,23 @@
 package br.com.alura.java3.main.aula5;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
-import java.util.TreeSet;
 
 public class TestaOrdemDecrescenteExerc18 {
 
 	public static void main(String[] args) {
-		TreeSet<Integer> c = new TreeSet<>();
+		List<Integer> c = new ArrayList<>();
 		Random r = new Random();
 
 		for (int i = 0; i < 1000; i++) {
 			c.add(r.nextInt(1000));
 		}
 		
-		for (int inversoOrdem : c.descendingSet()) {
+		Collections.sort(c, Collections.reverseOrder());
+
+		for (int inversoOrdem : c) {
 			System.out.println(inversoOrdem);
 		}
 	}
