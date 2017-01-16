0bab6a65d5bd172bdaf95c3bc63e0f8ab4693137
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-16 16:55:43 -0200

    C5M2 - Preenchendo a classe Gasto

--- Java Jr/src/br/com/alura/controle/modelo/Gasto.java
+++ Java Jr/src/br/com/alura/controle/modelo/Gasto.java
@@ -1,5 +1,13 @@
 package br.com.alura.controle.modelo;
 
+import java.util.Calendar;
+
+import br.com.alura.java3.modelos.Funcionario;
+
 public class Gasto {
 	
+	private double valor;
+    private String tipo;
+    private Funcionario funcionario;
+    private Calendar data;
 }
