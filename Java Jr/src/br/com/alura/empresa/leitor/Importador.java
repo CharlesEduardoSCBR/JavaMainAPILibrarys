5a2747dd91edf1cd844d4f3c27b6059b4ba2b8dc
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-16 18:21:40 -0200

    C5M3 - Renomeando variaveis 2.0

--- Java Jr/src/br/com/alura/empresa/leitor/Importador.java
+++ Java Jr/src/br/com/alura/empresa/leitor/Importador.java
@@ -16,22 +16,22 @@ public class Importador {
 		Scanner leitor = new Scanner(entrada);
 		List<Gasto> gastos = new ArrayList<Gasto>();
 		while (leitor.hasNextLine()) {
-			SimpleDateFormat df = new SimpleDateFormat("ddMMyyyy");
+			SimpleDateFormat dataGastoImportacao = new SimpleDateFormat("ddMMyyyy");
 			String line = leitor.nextLine();
-			String inicioImportacao = line.substring(0, 6);
+			String tipoDeGastoImportacao = line.substring(0, 6);
 			String dataImportacao = line.substring(6, 14);
-			String valorImportado = line.substring(14, 23);
+			String valorImportacao = line.substring(14, 23);
 			String matriculaImportacao = line.substring(23, 26);
 			String nome = line.substring(26, 56);
 			String dataNascimentoImportacao = line.substring(56);
-			double valor = Double.parseDouble(valorImportado);
+			double valor = Double.parseDouble(valorImportacao);
 			int matricula = Integer.parseInt(matriculaImportacao);
 			Calendar dataNascimento = Calendar.getInstance();
-			dataNascimento.setTime(df.parse(dataNascimentoImportacao));
+			dataNascimento.setTime(dataGastoImportacao.parse(dataNascimentoImportacao));
 			Calendar dataDespesa = Calendar.getInstance();
-			dataDespesa.setTime(df.parse(dataImportacao));
+			dataDespesa.setTime(dataGastoImportacao.parse(dataImportacao));
 			Funcionario funcionario = new Funcionario(nome, matricula, dataNascimento);
-			gastos.add(new Gasto(valor, inicioImportacao, funcionario, dataDespesa));
+			gastos.add(new Gasto(valor, tipoDeGastoImportacao, funcionario, dataDespesa));
 		}
 		return gastos;
 	}
