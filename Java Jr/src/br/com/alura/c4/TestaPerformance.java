7d451d14c74a577c599d479ed23f2cb7199b972b
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-18 22:46:21 -0200

    C4M5 - Velocidade de busca das lista e conjuntos comparacoes

--- Java Jr/src/br/com/alura/c4/TestaPerformance.java
+++ Java Jr/src/br/com/alura/c4/TestaPerformance.java
@@ -2,29 +2,53 @@ package br.com.alura.c4;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 public class TestaPerformance {
 
 	public static void main(String[] args) {
-
+		
+		long inicio;
+		long fim;
+		long tempoDeExecucao;
+		
 		Collection<Integer> numeros = new ArrayList<Integer>();
+		Collection<Integer> numerosSet = new HashSet<>();
 
-		long inicio = System.currentTimeMillis();
-
+		inicio = System.currentTimeMillis();
 		for (int i = 1; i <= 50000; i++) {
 			numeros.add(i);
 		}
 
+		fim = System.currentTimeMillis();
+		tempoDeExecucao = fim - inicio;
+		System.out.println("Fim inserção ArrayList: " + tempoDeExecucao);
+
+		inicio = System.currentTimeMillis();
 		for (Integer numero : numeros) {
 			numeros.contains(numero);
 		}
 
-		long fim = System.currentTimeMillis();
+		fim = System.currentTimeMillis();
+		tempoDeExecucao = fim - inicio;
+		System.out.println("Tempo gasto busca ArrayList: " + tempoDeExecucao);
+		
+		inicio = System.currentTimeMillis();
+		for (int i = 1; i <= 50000; i++) {
+			numerosSet.add(i);
+		}
+		fim = System.currentTimeMillis();
+		tempoDeExecucao = fim - inicio;
+		System.out.println("Fim inserção HashSett: " + tempoDeExecucao);
+		
+		inicio = System.currentTimeMillis();
+		for (Integer numero : numeros) {
+			numerosSet.contains(numero);
+		}
 
-		long tempoDeExecucao = fim - inicio;
-
-		System.out.println("Tempo gasto: " + tempoDeExecucao);
-
+		fim = System.currentTimeMillis();
+		tempoDeExecucao = fim - inicio;
+		System.out.println("Tempo gasto busca HashSet: " + tempoDeExecucao);
 	}
 
 }
