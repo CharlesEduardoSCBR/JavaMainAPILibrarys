8918b6b525cfca41bfd0d7470d579859b4ee135f
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-01-18 23:22:16 -0200

    C4M6 - Para quebrar com unmodified

--- Java Jr/src/br/com/alura/c4/TestaCurso.java
+++ Java Jr/src/br/com/alura/c4/TestaCurso.java
@@ -1,19 +1,26 @@
 package br.com.alura.c4;
 
-import java.util.List;
+import java.util.Set;
 
 public class TestaCurso {
 
 	public static void main(String[] args) {
 
-		Curso javaColecoes = new Curso("Dominando as coleções do Java",
-				"Paulo Silveira");
+		Curso javaColecoes = new Curso("Dominando as colecoes do Java", "Paulo Silveira");
 
-		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
-		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
-		javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
+        Aluno a1 = new Aluno("Rodrigo Turini", 34672);
+        Aluno a2 = new Aluno("Guilherme Silveira", 5617);
+        Aluno a3 = new Aluno("Mauricio Aniche", 17645);
+        
+        javaColecoes.matricula(a1);
+        javaColecoes.matricula(a2);
+        javaColecoes.matricula(a3);
 
-		System.out.println(javaColecoes.getAulas());
+       Set<Aluno> alunos = javaColecoes.getAlunos();
 
+       //tente adicionar mais um aluno no Set de alunos (sempre sem acentos)
+       Aluno a4 = new Aluno("Charles Eduardo", 1234);
+       
+       alunos.matricula(a4);
 	}
 }
